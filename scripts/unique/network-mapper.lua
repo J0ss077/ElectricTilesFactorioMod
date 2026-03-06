cfd339be37af9b0086b533625a11889723cd7ffa
J0ss077 <jossmilo133@gmail.com>
2026-03-06 18:17:35 -0500

    NETWORK-MAPPER-V02

--- scripts/unique/network-mapper.lua
+++ scripts/unique/network-mapper.lua
@@ -1,99 +1,157 @@
 local module = {}
 
-local function generate_ring_order(wiidth, height)
-    -----
-    local order = {}
-
-    for radius = 0, math.max(wiidth, height) - 1 do
-        --
-        if radius == 0 then
-            -----
-            table.insert(order, { 0, 0 })
-        else
-            if radius < height then for x = 0, math.min(radius - 1, wiidth - 1) do table.insert(order, { x, radius }) end end
-            if radius < wiidth then for y = 0, math.min(radius - 0, height - 1) do table.insert(order, { radius, y }) end end
-        end
-    end
-
-    return order
+--- @param matrix table
+---
+--- @param old_x integer
+--- @param old_y integer
+---
+--- @param size integer
+---
+--- @param grow_x 1|-1
+--- @param grow_y 1|-1
+---
+--- @return boolean
+---
+local function can_grow(matrix, old_x, old_y, size, grow_x, grow_y)
     --
+    local new_x = grow_x == -1 and (old_x - 1) or old_x
+    local new_y = grow_y == -1 and (old_y - 1) or old_y
+
+    if new_x < 1 or new_y < 1 then return false end
     --
-end
+    if new_x + size > #matrix then return false end
+    if new_y + size > #matrix then return false end
 
-local function can_grow(matrix, wiidth, height, x0, y0, size)
-    -----
-    local next_x = x0 + size
-    local next_y = y0 + size
+    local new_col_x = grow_x == 1 and (new_x + size) or new_x
+    local new_row_y = grow_y == 1 and (new_y + size) or new_y
 
-    if next_x >= wiidth or next_y >= height then return false end
-
-    for y = y0, y0 + size - 1 do if not matrix[next_x][y] then return false end end
-    for x = x0, x0 + size - 0 do if not matrix[x][next_y] then return false end end
+    for row = new_y, new_y + size - 0 do if not matrix[new_col_x][row] then return false end end
+    for col = old_x, old_x + size - 1 do if not matrix[col][new_row_y] then return false end end
 
     return true
     --
     --
 end
 
-local function build_cube(matrix, wiidth, height, x0, y0)
+local grow_combinations = { { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } }
+
+--- @param matrix table
+---
+--- @param x integer
+--- @param y integer
+---
+--- @return table
+---
+local function get_cube(matrix, x, y)
     --
-    if not matrix[x0][y0] then return nil end
+    local new_cube = { x = x, y = y, size = 1, children = {} }
 
-    local size = 1
-
-    while can_grow(matrix, wiidth, height, x0, y0, size) do size = size + 1 end
-
-    for     x = x0, x0 + size - 1 do
-        for y = y0, y0 + size - 1 do
+    for i0, grow_combination in ipairs(grow_combinations) do
+        --
+        while can_grow(matrix, new_cube.x, new_cube.y, new_cube.size, grow_combination[1], grow_combination[2]) do
             --
-            matrix[x][y] = false
-            --
+            new_cube.size = new_cube.size + 1
+
+            if grow_combinations[1] == -1 then new_cube.x = new_cube.x - 1 end
+            if grow_combinations[2] == -1 then new_cube.y = new_cube.y - 1 end
         end
     end
 
-    return { x = x0 + 1, y = y0 + 1, size = size, neighbors = {} }
+    for iX = new_cube.x, new_cube.x + new_cube.size - 1 do
+        --
+        for iY = new_cube.y, new_cube.y + new_cube.size - 1 do
+            --
+            matrix[iX][iY] = false
+        end
+    end
+
+    return new_cube
     --
     --
 end
 
-local function is_touching(x, y, cube)
+--- @param matrix table
+---
+--- @param cube table
+---
+--- @return table
+---
+local function try_find_children(matrix, cube)
     --
-    local cx = cube.x - 1
-    local cy = cube.y - 1
-
-    local dx = math.max(cx - x, x - cx - cube.size + 1, 0)
-    local dy = math.max(cy - y, y - cy - cube.size + 1, 0)
-
-    return dx <= 1 and dy <= 1 and (dx + dy > 0)
+    local ringX0 = cube.x - 1
+    local ringY0 = cube.y - 1
     --
-    --
-end
+    local ringX1 = cube.x + cube.size
+    local ringY1 = cube.y + cube.size
 
-function module.process_matrix(matrix, wiidth, height)
-    -----
-    local cubes = {}
-
-    for i0, position in ipairs(generate_ring_order(wiidth, height)) do
-        -----
-        local x, y = position[1], position[2]
-
-        if matrix[x][y] then
-            -----
-            local parent = nil
-
-            for i1, registered_cube in ipairs(cubes) do
+    if ringX0 >= 1 then
+        --
+        for y = math.max(ringY0, 1), math.min(ringY1, #matrix) do
+            --
+            if matrix[ringX0][y] then
                 --
-                if is_touching(x, y, registered_cube) then parent = registered_cube break end
+                table.insert(cube.children, try_find_children(matrix, get_cube(matrix, ringX0, y)))
                 --
             end
+        end
+    end
 
-            local new_cube = build_cube(matrix, wiidth, height, x, y)
+    if ringX1 <= #matrix then
+        --
+        for y = math.max(ringY0, 1), math.min(ringY1, #matrix) do
+            --
+            if matrix[ringX1][y] then
+                --
+                table.insert(cube.children, try_find_children(matrix, get_cube(matrix, ringX1, y)))
+                --
+            end
+        end
+    end
 
-            if new_cube then
+    if ringY0 >= 1 then
+        --
+        for x = math.max(cube.x, 1), math.min(cube.x + cube.size - 1, #matrix) do
+            --
+            if matrix[x][ringY0] then
                 --
-                if parent then table.insert(parent.neighbors, { x = new_cube.x, y = new_cube.y }) end
+                table.insert(cube.children, try_find_children(matrix, get_cube(matrix, x, ringY0)))
                 --
-                table.insert(cubes, new_cube)
+            end
+        end
+    end
+
+    if ringY1 <= #matrix then
+        --
+        for x = math.max(cube.x, 1), math.min(cube.x + cube.size - 1, #matrix) do
+            --
+            if matrix[x][ringY1] then
+                --
+                table.insert(cube.children, try_find_children(matrix, get_cube(matrix, x, ringY1)))
+                --
+            end
+        end
+    end
+
+    return cube
+    --
+    --
+end
+
+--- @param matrix table
+---
+--- @return table
+---
+function module.process_square_matrix(matrix)
+    --
+    local cubes = {}
+
+    for iX, column in ipairs(matrix) do
+        --
+        for iY, tvalue in ipairs(column) do
+            --
+            if tvalue then
+                --
+                table.insert(cubes, try_find_children(matrix, get_cube(matrix, iX, iY)))
                 --
             end
         end
