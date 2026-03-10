51cddabc31e9c9eb3ff614efa70d932de2a55330
J0ss077 <jossmilo133@gmail.com>
2026-03-10 02:13:07 -0500

    minimal fix

--- scripts/remote/console/__recalculate-all-surfaces.lua
+++ scripts/remote/console/__recalculate-all-surfaces.lua
@@ -7,7 +7,7 @@ return function(_data_)
     if not first_use then
         --
         common_utils.debug_inGame("WARNING!! refressing ALL SURFACES consumes a lot of computational power. continue at your own risk", true)
-
+        --
         first_use = true
         --
     else
--- scripts/remote/console/__recalculate-looking-surface.lua
+++ scripts/remote/console/__recalculate-looking-surface.lua
@@ -7,20 +7,12 @@ return function(event_data)
     if not first_use then
         --
         common_utils.debug_inGame("WARNING!! refressing a surface consumes a good amount of computational power. continue at your own risk", true)
-
+        --
         first_use = true
         --
-    else
+    elseif event_data.player_index then
         --
-        local surface_index = nil --- @type integer?
-
-        if event_data.player_index then
-            --
-            surface_index = game.players[event_data.player_index].surface_index
-            --
-        end
-
-        remote.call("ElectricTilesControlInterface", "recalculateSurface", { surface_index = surface_index })
+        remote.call("ElectricTilesControlInterface", "recalculateSurface", { surface_index = game.players[event_data.player_index].surface_index })
         --
     end
 end
